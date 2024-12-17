79e1350849a56adc1d84066478699a6884c2a12f
Eldeeb <omer84484@gmail.com>
2024-12-18 01:23:59 +0200

    Restore Computer file

---
+++ computer.py
@@ -0,0 +1,73 @@
+import random
+
+pieceScore = {"K": 0, "Q": 9, "R": 5, "B": 3, "N": 3, "p": 1}
+CHECKMATE = 1000
+STALEMATE = 0
+DEPTH = 0
+
+def findRandomMove(validMoves):
+    return random.choice(validMoves)
+
+def findBestMoveAlphaBeta(gs, validMoves, AIDepth=DEPTH):
+    global nextMove
+    nextMove = None
+    random.shuffle(validMoves)
+    alphaBeta(gs, validMoves, AIDepth, -CHECKMATE, CHECKMATE, 1 if gs.whiteToMove else -1)
+    return nextMove
+
+def alphaBeta(gs, validMoves, depth, alpha, beta, turnMultiplier):
+    global nextMove
+    if depth == 0:
+        return scoreBoard(gs)
+    if turnMultiplier == 1:
+        maxScore = -CHECKMATE
+        for move in validMoves:
+            gs.makeMove(move)
+            nextMoves = gs.getValidMoves()
+            score = alphaBeta(gs, nextMoves, depth - 1, alpha, beta, -turnMultiplier)
+            if score > maxScore:
+                maxScore = score
+                if depth == DEPTH:
+                    nextMove = move
+            gs.undoMove()
+            alpha = max(alpha, maxScore)
+            if alpha >= beta:
+                break
+        return maxScore
+    else:
+        minScore = CHECKMATE
+        for move in validMoves:
+            gs.makeMove(move)
+            nextMoves = gs.getValidMoves()
+            score = alphaBeta(gs, nextMoves, depth - 1, alpha, beta, -turnMultiplier)
+            if score < minScore:
+                minScore = score
+                if depth == DEPTH:
+                    nextMove = move
+            gs.undoMove()
+            beta = min(beta, minScore)
+            if alpha >= beta:
+                break
+        return minScore
+
+def scoreBoard(gs):
+    if gs.checkmate:
+        if gs.whiteToMove:
+            return -CHECKMATE
+        else:
+            return CHECKMATE
+    elif gs.stalemate:
+        return STALEMATE
+    score = scoreMaterial(gs.board)
+    return score
+
+# Score the board based on material
+def scoreMaterial(board):
+    score = 0
+    for row in board:
+        for square in row:
+            if square[0] == 'w':
+                score += pieceScore[square[1]]
+            elif square[0] == 'b':
+                score -= pieceScore[square[1]]
+    return score
